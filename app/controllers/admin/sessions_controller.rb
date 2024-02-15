8a0ce8327cc7e8ad29793b7eb3de847b1c88a5f0
k-0107 <k.tkm0107@gmail.com>
2024-02-15 11:28:28 +0000

    [add]devise_login

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  feature-membership_admin_side
   
   def after_sign_in_path_for(resource)
     admin_root_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
   end
   # before_action :configure_sign_in_params, only: [:create]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  namespace :public do
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  scope module: :public do
     root :to => 'homes#top'
     get 'about' => 'home#about'
     resources :items, only: [:index, :show]
@@ -21,17 +32,7 @@ Rails.application.routes.draw do
     end
     resources :addresses, only: [:create, :index, :edit, :update, :destroy]
   end
-  
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
 
-  # URL /admin/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
   
   namespace :admin do
     root :to => 'homes#top'
