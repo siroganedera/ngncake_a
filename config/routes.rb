98abe675210b430ee61b0ab0da8edbfe77ca9976
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-02-15 11:26:42 +0000

    [add]font awessome

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope module: :public do
     root :to => 'homes#top'
-    get 'about' => 'home#about'
+    get 'about' => 'homes#about'
     resources :items, only: [:index, :show]
     get 'customers/my_page', to: 'customers#show'
     get 'customers/information/edit', to: 'customers#edit'
