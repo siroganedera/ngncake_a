92296ff834554ef0db34aa880c81c546219dd1ca
150042192+docdog158@users.noreply.github.com <ec2-user@ip-172-31-31-106.ap-northeast-1.compute.internal>
2024-02-15 05:52:20 +0000

    genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,29 @@
 class Admin::GenresController < ApplicationController
   def create
+    @genre= Genre.new
+    @genre.user_id = current_user.id
+    if@genre.save
+      redirect_to genre_path(@genre), notice: "ジャンルを追加しました"
+    else
+      @genre=Genre.all
+    end
   end
 
   def index
+    @genre = Genre.all
+    @genre = Genre.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @book.update(genre_params)
+      redirect_to genre_path(@genre), notice: "ジャンル名を変更しました。"
+    else
+      render "edit"
+    end
   end
 end
