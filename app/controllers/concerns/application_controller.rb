b465b16b1f85a232c035fecb1c4ffb8646717ff6
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-02-15 11:29:19 +0000

    修正

--- app/controllers/concerns/application_controller.rb
+++ app/controllers/concerns/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
-  private
 
-  def after_sign_in_path_for(resource)
-    user_path(resource)
-  end
-
-  def after_sign_out_path_for(resource)
-    root_path
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end
 end
