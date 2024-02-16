bb715eb06331ba749230746c7ed74ae7d4bee501
150042192+docdog158@users.noreply.github.com <ec2-user@ip-172-31-31-106.ap-northeast-1.compute.internal>
2024-02-16 04:08:00 +0000

    test

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,16 +1,16 @@
 class Admin::GenresController < ApplicationController
   def create
-    @genre= Genre.new
-    @genre.admin_id = current_admin.id
+    @genre = Genre.new(genre_params)
     if@genre.save
-      redirect_to genre_path(@genre), notice: "ジャンルを追加しました"
+      redirect_to admin_genres_path(@genre)#, notice: "ジャンルを追加しました"
     else
-      @genre=Genre.all
+      @genres=Genre.all
+      render 'index'
     end
   end
 
   def index
-    @genre = Genre.all
+    @genres = Genre.all
     @genre = Genre.new
   end
 
@@ -21,9 +21,17 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
-      redirect_to genre_path(@genre), notice: "ジャンル名を変更しました。"
+      redirect_to admin_genres_path(@genre)#, notice: "ジャンル名を変更しました。"
     else
       render "edit"
     end
   end
+  
+    private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
+  
 end
