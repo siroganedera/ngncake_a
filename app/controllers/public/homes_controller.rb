b2440cbfe36c85145e5028a6cae758c15b9d2239
150042192+docdog158@users.noreply.github.com <ec2-user@ip-172-31-31-106.ap-northeast-1.compute.internal>
2024-02-17 01:38:46 +0000

    top=page

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
-    @genres = genre.all
+    @genres = Genre.all
+    @recent_items = Item.order(created_at: :desc).limit(4)
   end
 
   def about
