dd5e3c543bc52b4f17e24b00d16c72895dc4191f
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-02-21 09:26:15 +0000

    [finish]items

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
     validates :name
     validates :introduction
     validates :price
-    validates :image
+    validates :ima
   end
 
   def get_image(width,height)
