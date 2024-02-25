87023ac649e7e5e99d721765259e75146803ace3
150042192+docdog158@users.noreply.github.com <ec2-user@ip-172-31-31-106.ap-northeast-1.compute.internal>
2024-02-25 04:27:46 +0000

    連動ステータス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,16 +4,27 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_detail = OrderDetail.find(params[:id]) 
     @order_details = @order.order_details.all
+    @total_price = calculate_total_price(@order.order_details)
   end
 
   def update
     @order = Order.find(params[:id])
+    @order_details = @order.order_details
     if @order.update(order_params)
-      redirect_to admin_order_path(@order)
-    else
-      render :show, locals: { order: @order } 
+      @order_details.update_all(making_status: "waiting_for_production") if @order.status == "payment_confirmed"
     end
+      redirect_to admin_order_path(@order)
+      
   end
+  
+  def calculate_total_price(order_detail)
+    total_price = 0
+    order_detail.each do |order_detail|
+      total_price += order_detail.price * order_detail.amount
+    end
+    return total_price
+  end
+  
   
   def order_params
     params.require(:order).permit(:status)
--- app/controllers/admin/orders_details_controller.rb
+++ app/controllers/admin/orders_details_controller.rb
@@ -1,14 +1,24 @@
 class Admin::OrdersDetailsController < ApplicationController
   before_action :authenticate_admin!
 
-  def update
+  def update 
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_details = @order.order_details.all
+    
+   is_updated = true
     if @order_detail.update(order_detail_params)
-      redirect_to admin_order_path(@order_detail.order)
-    else
-      render :show, locals: { order_detail: @order_detail }
+       @order.update(status: "in_production") if @order_detail.making_status == "in_production"
+       @order_details.each do |order_detail| 
+          if order_detail.making_status != "production_completed"
+            is_updated = false 
+          end
+       end
+       @order.update(status: "preparing_for_shipment") if is_updated
     end
+       redirect_to admin_order_path(@order)
   end
+  
 
   private
 
