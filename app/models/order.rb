d25a3b83a3fbd047bbd133d5e6b122ef77622200
150042192+docdog158@users.noreply.github.com <ec2-user@ip-172-31-31-106.ap-northeast-1.compute.internal>
2024-02-25 04:42:37 +0000

    rendou

--- app/models/order.rb
+++ app/models/order.rb
@@ -22,8 +22,6 @@ class Order < ApplicationRecord
   
   private
   
-#  validates :,presence:true
-#  validates :,presence:true,length:{maximum:200}
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { awaiting_payment: 0, payment_confirmed: 1, in_production: 2, preparing_for_shipment: 3, shipped: 4 }
 end
